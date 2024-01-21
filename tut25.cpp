88d324e69445937df4c40f978fda5b3190561151
dikshamakode <dikshamakode26@gmail.com>
2024-01-21 16:23:22 +0530

    modified:   tut25.cpp 	new file:   tut25.exe

--- tut25.cpp
+++ tut25.cpp
@@ -1,21 +1,37 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-class Employee{
+class Employee
+{
     int id;
     int salary;
-    public:
-    void setId(void){
-        cout<<"enter the id of employee";
-        cin>>id;
+
+public:
+    void setId(void)
+    {
+        salary = 122;
+        cout << "enter the id of employee";
+        cin >> id;
     }
 
-    void getId(){
+    void getId(void)
+    {
+        cout << "the id of this employee is " << id << endl;
+    }
+};
+
+int main()
+{
+    /*Employee harry, rohan, rishi, shruti;
+    harry.setId();
+    harry.getId();*/
+    Employee fb[4];
+    for (int i = 0; i < 4; i++)
+    {
+        fb[i].setId();
+        fb[i].getId();
         
     }
 
-}
-
-int main(){
     return 0;
 }
