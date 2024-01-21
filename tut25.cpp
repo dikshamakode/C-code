ecdbde5fff51504e1f520a68640c999fd4a9b7a7
dikshamakode <dikshamakode26@gmail.com>
2024-01-21 14:56:17 +0530

    modified:   tut25.cpp

---
+++ tut25.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Employee{
+    int id;
+    int salary;
+    public:
+    void setId(void){
+        cout<<"enter the id of employee";
+        cin>>id;
+    }
+
+    void getId(){
+        
+    }
+
+}
+
+int main(){
+    return 0;
+}
