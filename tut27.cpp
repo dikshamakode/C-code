c9417f46270195a4ee72a80888aa0b251d34dbd4
dikshamakode <dikshamakode26@gmail.com>
2024-01-22 22:02:13 +0530

    Create tut27.cpp

---
+++ tut27.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+class Complex{
+    int a, b;
+    public:
+        void setNumber(int n1, int n2){
+            a = n1;
+            b = n2;
+        }
+
+        
+        void printNumber(){
+            cout<<"your number is "<<a<<" + "<<b<<"i"<<endl;
+
+        }
+};
+class Calculator{
+    public:
+        int add(int a , int b){
+            return (a + b);
+        }
+
+        int sumcomplex(Complex o1, Complex o2){
+            return o1.a + o2.a
+        }
+}
+
+
+int main(){
+
+    return 0;
+}
