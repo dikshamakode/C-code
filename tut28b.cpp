d69c2a8c5581dd7160498676a4d1f71ba3a0201d
dikshamakode <dikshamakode26@gmail.com>
2024-02-03 22:48:45 +0530

    modified:   tut28b.cpp

--- tut28b.cpp
+++ tut28b.cpp
@@ -1,15 +1,15 @@
 #include<iostream>
 using namespace std;
-int main(){
+
     class c1{
-        int val;
+        int val1;
         public:
             void intdata(int a){
-                val =a;
+                val1 =a;
         }
 
         void display(void){
-            cout<< val << endl;
+            cout<< val1 << endl;
         }
 
     };
@@ -24,8 +24,21 @@ class c2{
             cout<< val2 << endl;
         }
 
-    };
+};
+void exchange(c1 & x, c2& y){
+    int tmp = x.val1;
+    x.val1 = y.val2;
+    y.val2 = tmp;
+}
 
+int main(){
+    c1 oc1;
+    c2 oc2;
 
+    oc1.indata(34);
+    oc2.indata(34);
+    exchange(oc1, oc2);
+    cout<<" the value of c1 after exchangingbecome: "<<oc1.display();
+    cout<<" the value of c2 after exchangingbecome: "<<oc2.display();
     return 0;
 }
