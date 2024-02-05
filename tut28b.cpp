5902c634c8a7e244239b6a7a101a6982171b6f42
dikshamakode <dikshamakode26@gmail.com>
2024-02-06 01:11:01 +0530

    modified:   tut28b.cpp 	new file:   tut28b.exe

--- tut28b.cpp
+++ tut28b.cpp
@@ -3,8 +3,9 @@ using namespace std;
 class c2;
     class c1{
         int val1;
+        friend void exchange(c1 &, c2 &);
         public:
-            void intdata(int a){
+            void indata(int a){
                 val1 =a;
         }
 
@@ -15,8 +16,9 @@ class c2;
     };
 class c2{
         int val2;
+        friend void exchange(c1 &, c2 &);
         public:
-            void intdata(int a){
+            void indata(int a){
                 val2 =a;
         }
 
@@ -36,7 +38,7 @@ int main(){
     c2 oc2;
 
     oc1.indata(34);
-    oc2.indata(34);
+    oc2.indata(67);
     exchange(oc1, oc2);
     cout<<" the value of c1 after exchanging become: ";
     oc1.display();
