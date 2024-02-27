62dbfddd31478b5bc6ce05ada3417ffbc402d7fd
dikshamakode <dikshamakode26@gmail.com>
2024-02-27 23:59:40 +0530

    modified:   tut30b.cpp

--- tut30b.cpp
+++ tut30b.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 using namespace std;
 class Point{
+    int x, y;
+    public:
+        Point(int a, int b){
+            
+        }
     int main (){
         return 0;
     }
